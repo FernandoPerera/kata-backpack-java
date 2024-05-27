24e134a15c532ac608399f9e2a5b3b11a4b18fef
FernandoPerera <fernando02perera@bitboxonline.com>
2024-05-27 08:44:47 +0100

    :white_check_mark: should_cannot_store_an_item_if_backpack_is_full

--- src/main/java/com/kata/backpack/models/Backpack.java
+++ src/main/java/com/kata/backpack/models/Backpack.java
@@ -1,6 +1,7 @@
 package com.kata.backpack.models;
 
 import com.kata.backpack.common.Error;
+import com.kata.backpack.errors.CannotStoreItem;
 import io.vavr.control.Either;
 
 import java.util.ArrayList;
@@ -10,11 +11,18 @@ public class Backpack {
 
     private List<Item> items = new ArrayList<Item>();
 
+    private final int BACKPACK_LIMIT_CAPACITY = 8;
+
     public Backpack() {}
 
     public Either<Error, Backpack> store(Item item) {
-        items.add(item);
-        return Either.right(this);
+
+        boolean backpackIsNotFull = items.size() < BACKPACK_LIMIT_CAPACITY;
+        if (backpackIsNotFull) {
+            items.add(item);
+            return Either.right(this);
+        }
+        return Either.left(new CannotStoreItem("Cannot store more items, backpack is full !!"));
     }
 
     public List<Item> getItems() {
